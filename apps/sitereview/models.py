e33da09123ff5cbbe902dac4f510355041f53e7f
Darby Clement <darbyclement@berkeley.edu>
2020-07-29 17:35:10 -0700

    added api models etc

--- apps/sitereview/models.py
+++ apps/sitereview/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Website(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    domain = models.CharField(max_length = 100)
+
+class Page(models.Model):
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    
+    pageURL = models.CharField(max_length=100)
+    visitorCount = models.IntegerField()
+    timeOnPage = models.IntegerField()
+    creationDate = models.CharField(max_length=100)
+    reviewerScore = models.IntegerField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length = 100)
+
+
+
