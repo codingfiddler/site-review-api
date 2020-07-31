a51552208a63d02dc144eef60031a9a023769b9d
Tim Van Cleave <tjvancleave@spscommerce.com>
2020-07-30 21:54:17 -0500

    Added comments to pages

--- apps/sitereview/serializers.py
+++ apps/sitereview/serializers.py
@@ -16,14 +16,21 @@ class WebsiteSerializer(serializers.ModelSerializer):
         # All fields will be shown
         fields = '__all__'
 
-class PageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Page
-        # All fields will be shown
-        fields = '__all__'
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        # All fields will be shown
+        fields = '__all__'
+
+
+class PageSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
+
+    def get_comments(self, obj):
+        comments = obj.comment_set.all()
+        return CommentSerializer(comments, many=True, read_only=True).data
+
+    class Meta:
+        model = Page
         # All fields will be shown
         fields = '__all__'
