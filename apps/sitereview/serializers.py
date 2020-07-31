f11669eb482a9f4526a44d093907e057746b94a1
Darby Clement <darbyclement@berkeley.edu>
2020-07-30 22:13:20 -0700

    added relationships

--- apps/sitereview/serializers.py
+++ apps/sitereview/serializers.py
@@ -4,13 +4,22 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    websites = serializers.SerializerMethodField()
+    def get_websites(self, obj):
+        websites = obj.website_set.all()
+        return WebsiteSerializer(websites, many=True, read_only=True).data
+
     class Meta:
         model = User
         # We only want to display the below fields when returning data
         # You can still send any of the hidden fields in payloads
-        fields = ['username', 'user_permissions', 'first_name', 'last_name', 'email', 'id']
+        fields = ['username', 'user_permissions', 'first_name', 'last_name', 'email', 'id', 'websites']
 
 class WebsiteSerializer(serializers.ModelSerializer):
+    pages = serializers.SerializerMethodField()
+    def get_pages(self, obj):
+        pages = obj.page_set.all()
+        return PageSerializer(pages, many=True, read_only=True).data
     class Meta:
         model = Website
         # All fields will be shown
@@ -25,7 +34,6 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class PageSerializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField()
-
     def get_comments(self, obj):
         comments = obj.comment_set.all()
         return CommentSerializer(comments, many=True, read_only=True).data
